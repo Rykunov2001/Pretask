c86c3af6c41fa98eb44899d7aa514888ca757ee1
IlyaRykunov <78876268+Rykunov2001@users.noreply.github.com>
2021-10-03 16:36:39 +0300

    added auto comments

--- Euclidean.cs
+++ Euclidean.cs
@@ -6,11 +6,21 @@ using System.Diagnostics;
 
 namespace PreTask
 {
+    /// <summary>
+    /// Class Euclidean calculates GCD
+    /// </summary>
     public class Euclidean
     {
-        //private Stopwatch stopwatch = new Stopwatch();
-
-        public static int FindGDC(int a, int b, ref Stopwatch stopwatch)
+        /// <summary>
+        /// Euclid's method for finding GCD.
+        /// Takes 3 parameters, 2 numbers and a time counter,
+        /// returns the greatest common divisor
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="stopwatch"></param>
+        /// <returns></returns>
+        public static int FindGCD(int a, int b, ref Stopwatch stopwatch)
         {
             if (b < 0 || b < 0)
                 throw new Exception("The number must not be negative");
@@ -25,25 +35,67 @@ namespace PreTask
             }            
             return a;
         }
-        public static int FindGDC(int a,int b,int c, ref Stopwatch stopwatch)
+        /// <summary>
+        /// Euclid's method for finding GCD.
+        /// Takes 4 parameters, 3 numbers and a time counter,
+        /// returns the greatest common divisor
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="stopwatch"></param>
+        /// <returns></returns>
+        public static int FindGCD(int a,int b,int c, ref Stopwatch stopwatch)
         {
-            int result = FindGDC(a, b, ref stopwatch);
-            result = FindGDC(result, c, ref stopwatch);
+            int result = FindGCD(a, b, ref stopwatch);
+            result = FindGCD(result, c, ref stopwatch);
             return result;
         }
-        public static int FindGDC(int a,int b,int c,int d, ref Stopwatch stopwatch)
+        /// <summary>
+        /// Euclid's method for finding GCD.
+        /// Takes 5 parameters, 4 numbers and a time counter,
+        /// returns the greatest common divisor
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <param name="stopwatch"></param>
+        /// <returns></returns>
+        public static int FindGCD(int a,int b,int c,int d, ref Stopwatch stopwatch)
         {
-            int result = FindGDC(a, b, c, ref stopwatch);
-            result = FindGDC(result, d, ref stopwatch);
+            int result = FindGCD(a, b, c, ref stopwatch);
+            result = FindGCD(result, d, ref stopwatch);
             
             return result;
         }
-        public static int FindGDC(int a, int b, int c, int d,int e, ref Stopwatch stopwatch)
+        /// <summary>
+        /// Euclid's method for finding GCD.
+        /// Takes 6 parameters, 5 numbers and a time counter,
+        /// returns the greatest common divisor
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        /// <param name="stopwatch"></param>
+        /// <returns></returns>
+        public static int FindGCD(int a, int b, int c, int d,int e, ref Stopwatch stopwatch)
         {
-            int result = FindGDC(a, b, c, d, ref stopwatch);
-            result = FindGDC(result, e, ref stopwatch);
+            int result = FindGCD(a, b, c, d, ref stopwatch);
+            result = FindGCD(result, e, ref stopwatch);
             return result;
         }
+        /// <summary>
+        ///  Binary method for finding GCD.
+        ///  Takes 3 parameters, 2 numbers and a time counter,
+        ///  returns the greatest common divisor
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="stopwatch"></param>
+        /// <returns></returns>
         public static int FindBinaryGDC(int a,int b, ref Stopwatch stopwatch)
         {
             if (b < 0 || b < 0)
@@ -73,6 +125,21 @@ namespace PreTask
                     return FindBinaryGDC((b - a) / 2, a, ref stopwatch);
             }
             
+        }
+        /// <summary>
+        /// Prepares a tuple of data for plotting a histogram.
+        /// Takes 2 parameters, 2 time collections,
+        /// returns a tuple
+        /// </summary>
+        /// <param name="stopwatch1"></param>
+        /// <param name="stopwatch2"></param>
+        /// <returns></returns>
+        public (Stopwatch[], Stopwatch[]) GetGistogramm(Stopwatch[] stopwatch1, Stopwatch[] stopwatch2)
+        {
+            (Stopwatch[], Stopwatch[]) TimeTuple;
+            TimeTuple.Item1 = stopwatch1;
+            TimeTuple.Item2 = stopwatch2;
+            return TimeTuple;
         }
 
     }
