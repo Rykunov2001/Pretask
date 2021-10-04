9dbbc301171a6417afb57c2bffbbd5fd3e20a2e3
IlyaRykunov <78876268+Rykunov2001@users.noreply.github.com>
2021-10-04 14:19:59 +0300

    Fixed the error in the names of the methods

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace PreTask
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Console.WriteLine(Euclidean.FindGDC( -23 , 23,ref stopwatch));
+            Console.WriteLine(Euclidean.FindGCD( -23 , 23,ref stopwatch));
             Console.WriteLine( stopwatch.Elapsed);
             stopwatch.Restart();
             Console.WriteLine(Euclidean.FindBinaryGDC(1568, 4, ref stopwatch));
